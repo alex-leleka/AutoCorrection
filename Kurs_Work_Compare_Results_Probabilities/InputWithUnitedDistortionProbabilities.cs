397f29e9d0447bc8e980988625357d236d6cb205
alex leleka <aleksstork@gmail.com>
2015-01-20 00:07:08 +0200

    add getter for inputBitMap

--- Kurs_Work_Compare_Results_Probabilities/InputWithUnitedDistortionProbabilities.cs
+++ Kurs_Work_Compare_Results_Probabilities/InputWithUnitedDistortionProbabilities.cs
@@ -7,26 +7,31 @@ namespace Diplom_Work_Compare_Results_Probabilities
 {
     class InputWithUnitedDistortionProbabilities
     {
-        private double[] distortionToZeroProbability;
-        private double[] distortionToOneProbability;
-        private double[] distortionToInverseProbability;
-        private double[] probalityZero;
-        private double[] distortionToZeroProbabilityWithUnited;
-        private double[] distortionToOneProbabilityWithUnited;
-        private double[] distortionToInverseProbabilityWithUnited;
-        private int[] inputBitMap;
+        private readonly double[] _distortionToZeroProbability;
+        private readonly double[] _distortionToOneProbability;
+        private readonly double[] _distortionToInverseProbability;
+        private readonly double[] _probalityZero;
+        private readonly double[] _distortionToZeroProbabilityWithUnited;
+        private readonly double[] _distortionToOneProbabilityWithUnited;
+        private readonly double[] _distortionToInverseProbabilityWithUnited;
+        private readonly int[] _inputBitMap;
 
         public InputWithUnitedDistortionProbabilities(double[] distortionToZeroProbability, double[] distortionToOneProbability, double[] distortionToInverseProbability, double[] probalityZero, double[] distortionToZeroProbabilityWithUnited, double[] distortionToOneProbabilityWithUnited, double[] distortionToInverseProbabilityWithUnited, int[] inputBitMap)
         {
             // TODO: Complete member initialization
-            this.distortionToZeroProbability = distortionToZeroProbability;
-            this.distortionToOneProbability = distortionToOneProbability;
-            this.distortionToInverseProbability = distortionToInverseProbability;
-            this.probalityZero = probalityZero;
-            this.distortionToZeroProbabilityWithUnited = distortionToZeroProbabilityWithUnited;
-            this.distortionToOneProbabilityWithUnited = distortionToOneProbabilityWithUnited;
-            this.distortionToInverseProbabilityWithUnited = distortionToInverseProbabilityWithUnited;
-            this.inputBitMap = inputBitMap;
+            this._distortionToZeroProbability = distortionToZeroProbability;
+            this._distortionToOneProbability = distortionToOneProbability;
+            this._distortionToInverseProbability = distortionToInverseProbability;
+            this._probalityZero = probalityZero;
+            this._distortionToZeroProbabilityWithUnited = distortionToZeroProbabilityWithUnited;
+            this._distortionToOneProbabilityWithUnited = distortionToOneProbabilityWithUnited;
+            this._distortionToInverseProbabilityWithUnited = distortionToInverseProbabilityWithUnited;
+            this._inputBitMap = inputBitMap;
+        }
+
+        int GetBitMappedVariableIndex(int inputIndex)
+        {
+            return _inputBitMap[inputIndex];
         }
     }
 }
