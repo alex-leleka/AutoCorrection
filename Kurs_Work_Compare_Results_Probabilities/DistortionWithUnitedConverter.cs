0331eb98e069192d66b6a58a56a1394ff1df2762
alex leleka <aleksstork@gmail.com>
2015-04-17 01:17:34 +0300

    update distconverter

--- Kurs_Work_Compare_Results_Probabilities/DistortionWithUnitedConverter.cs
+++ Kurs_Work_Compare_Results_Probabilities/DistortionWithUnitedConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,11 @@ namespace Diplom_Work_Compare_Results_Probabilities
     class DistortionWithUnitedConverter
     {
         private InputWithUnitedDistortionProbabilities _inpDist;
+        private readonly ReadOnlyCollection<int> _firstLevelInputsTargets;
 
+        private double[] _idpDistortionToZeroProbability;// = new double[GetSecondLevelInputsCount()];
+        private double[] _idpDistortionToOneProbability;// = new double[GetSecondLevelInputsCount()];
+        private double[] _idpDistortionToInverseProbability;// = new double[GetSecondLevelInputsCount()];
+        private double[] _idpProbalityZero;// = new double[GetSecondLevelInputsCount()];
     }
 }
