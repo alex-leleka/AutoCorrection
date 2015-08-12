1111fa460718160d00a3b79435a1afe4c4365262
alex leleka <aleksstork@gmail.com>
2015-08-12 19:15:06 +0300

    add statistics worker

---
+++ Kurs_Work_Compare_Results_Probabilities/StatCollector/StatisticsWorker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Diplom_Work_Compare_Results_Probabilities.TruthTable;
+
+namespace Diplom_Work_Compare_Results_Probabilities.Statistics
+{
+    /// <summary>
+    /// Get boolean function and distortin as parameters.
+    /// Return autocorrection result.
+    /// Only for logical networks with united inputs.
+    /// </summary>
+    class StatisticsWorker
+    {
+        private BooleanFuntionWithInputDistortion _bfWithInpDist;
+        private InputWithUnitedDistortionProbabilities _inpDistProb;
+
+        public StatisticsWorker(BooleanFuntionWithInputDistortion bfWithInpDist, InputWithUnitedDistortionProbabilities inpDistProb)
+        {
+            _bfWithInpDist = bfWithInpDist;
+            _inpDistProb = inpDistProb;
+        }
+
+        public Dictionary<int, double> GetResult()
+        {
+            var pCalc = new ProbabilitiesCorrLogicNetWithUnitedInputs(_inpDistProb, _bfWithInpDist);
+            var pCorrectResult = pCalc.GetCorrectResultProbability();
+            return pCorrectResult;
+        }
+    }
+}
