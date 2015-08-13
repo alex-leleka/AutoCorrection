1eaef88eb27dfd8d42f0f355766a4baa9d5ffdea
alex leleka <aleksstork@gmail.com>
2015-08-13 14:03:00 +0300

    optimize statistics writer

--- Kurs_Work_Compare_Results_Probabilities/StatCollector/StatisticsWriter.cs
+++ Kurs_Work_Compare_Results_Probabilities/StatCollector/StatisticsWriter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Diplom_Work_Compare_Results_Probabilities.StatCollector
 {
@@ -12,6 +13,18 @@ namespace Diplom_Work_Compare_Results_Probabilities.StatCollector
     /// </summary>
     class StatisticsWriter
     {
+        String _fileName;
+        StreamWriter _sw;
+        DisposableDelegate _swDisposer;
+        public StatisticsWriter(String fileNamePrefix = "")
+        {
+            _fileName = fileNamePrefix + string.Format("Stats-{0:yyyy-MM-dd_hh-mm-ss-tt}.csv",
+                DateTime.Now);
+            // Open stream only once in constructor
+            _sw = new StreamWriter(_fileName, true);
+            // Colse stream when _swDisposer leave scope
+            _swDisposer = new DisposableDelegate(() => _sw.Close());
+        }
         public void WiteStatistics(StatisticsWorker worker)
         {
             var resultsProbs = worker.GetResult();
@@ -25,9 +38,47 @@ namespace Diplom_Work_Compare_Results_Probabilities.StatCollector
             }
         }
 
-        private void WriteLine(string p)
+        // contains exception handling, couldn't be inline
+        private void WriteLine(string line)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _sw.WriteLine(line);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("StatisticsWriter: Error while witing statistics:" + e.Message);
+            }
+        }
+
+        private void WriteLineFast(string line)
+        {
+            _sw.WriteLine(line);
+        }
+    }
+
+    public class DisposableDelegate : IDisposable
+    {
+        private Action dispose;
+
+        public DisposableDelegate(Action dispose)
+        {
+            if (dispose == null)
+            {
+                throw new ArgumentNullException("dispose");
+            }
+
+            this.dispose = dispose;
+        }
+
+        public void Dispose()
+        {
+            if (this.dispose != null)
+            {
+                Action d = this.dispose;
+                this.dispose = null;
+                d();
+            }
         }
     }
 }
