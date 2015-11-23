6b9c9d5b65a5a70b2cc3e78fa27426ffee1d6b6f
alex leleka <aleksstork@gmail.com>
2015-11-24 00:03:11 +0200

    SubfunctionPrototype

---
+++ SubfunctionPrototype/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+using Diplom_Work_Compare_Results_Probabilities;
+using DotNetUtils;
+using System.Windows.Forms;
+using Diplom_Work_Compare_Results_Probabilities.TruthTable;
+
+namespace SubfunctionPrototype
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int _FixedBitsCount = 2;
+
+        private G4Probability CalculateFunctionDistortion(BooleanFuntionWithInputDistortion bf, InputDistortionProbabilities idp)
+        {
+            var pCalc = new ProbabilitiesGxyCalc(bf, idp);
+            const int binaryFunctionResultCount = 2;
+            Gprobabilites[] prob = new Gprobabilites[binaryFunctionResultCount];
+            for (int i = 0; i < binaryFunctionResultCount; i++)
+            {
+                prob[i] = pCalc.GetGprobabilitesResult(i.ToBinary(1));
+            }
+            return new G4Probability(prob);
+        }
+
+        private G4Probability[] GenerateSubfunctions(BooleanFuntionWithInputDistortion bf, InputDistortionProbabilities idp)
+        {
+            int newBitsCount = idp.ZeroProbability.Length - _FixedBitsCount;
+            // generate new InputDistortionProbabilities
+            var reducedIdp = GeberateReducedInpDistProbs(idp, newBitsCount);
+            int subfunctionsCount = 1 << _FixedBitsCount;
+            var reducedBfArr = new BooleanFuntionWithInputDistortion[subfunctionsCount];
+            // Generate subfunctions
+            for (int i = 0; i < subfunctionsCount; i++)
+            {
+                reducedBfArr[i] = GenerateReducedFunction(bf, newBitsCount, i);
+            }
+            // calculate sunfuncions autocorrection (G4 vector)
+            G4Probability[] probs = new G4Probability[subfunctionsCount];
+            for (int i = 0; i < subfunctionsCount; i++)
+            {
+                probs[i] = CalculateFunctionDistortion(reducedBfArr[i], reducedIdp);
+            }
+            return probs;
+
+        }
+
+        private BooleanFuntionWithInputDistortion GenerateReducedFunction(BooleanFuntionWithInputDistortion bf, int newBitsCount, int fixedOperand)
+        {
+            throw new NotImplementedException();
+        }
+
+        private InputDistortionProbabilities GeberateReducedInpDistProbs(InputDistortionProbabilities idp, int newBitsCount)
+        {
+            double[] distortionToZeroProbability = new double[newBitsCount];
+            double[] distortionToOneProbability = new double[newBitsCount];
+            double[] distortionToInverseProbability = new double[newBitsCount];
+            double[] zeroProbability = new double[newBitsCount];
+            for (int i = 0; i < newBitsCount; i++)
+            {
+                distortionToZeroProbability[i] = idp.DistortionToZeroProbability[i];
+                distortionToOneProbability[i] = idp.DistortionToOneProbability[i];
+                distortionToInverseProbability[i] = idp.DistortionToInverseProbability[i];
+                zeroProbability[i] = idp.ZeroProbability[i];
+            }
+            var newIdp = new InputDistortionProbabilities(distortionToZeroProbability, distortionToOneProbability, 
+                distortionToInverseProbability, zeroProbability);
+            return newIdp;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
