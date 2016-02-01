c39f6d88c8bc980dab506a431b7a7de947d4a5f1
alex-leleka <aleksstork@gmail.com>
2016-02-02 00:21:51 +0200

    update unittest

--- ProbabilitiesCalculatorTester/GxIndexesCombinationTest.cs
+++ ProbabilitiesCalculatorTester/GxIndexesCombinationTest.cs
@@ -62,7 +62,7 @@ namespace ProbabilitiesCalculatorTester
         }
 
         [TestMethod]
-        public void TestIncrementValue()
+        public void TestIncrementValue1()
         {
             int[] bounds = new[] { 2, 2 };
             GxIndexesCombination gxIC = new GxIndexesCombination(bounds);
@@ -91,6 +91,81 @@ namespace ProbabilitiesCalculatorTester
             CopyIndexesToArray(ref actualIncremented, gxIC);
             CollectionAssert.AreEqual(expectedIndxs, actualIncremented, "Increment #5 failed. Indexes:" 
                 + actualIncremented[0] + " " + +actualIncremented[1]);
+        }
+
+        [TestMethod]
+        public void TestIncrementValue2()
+        {
+            int[] bounds = new[] { 3, 3, 2 };
+            GxIndexesCombination gxIC = new GxIndexesCombination(bounds);
+            int[] expectedIndxs = new int[] { 0, 0, 0 };
+            int[] actualIncremented = new int[] { 0, 0, 0 };
+            CopyIndexesToArray(ref actualIncremented, gxIC);
+            CollectionAssert.AreEqual(actualIncremented, actualIncremented, "Increment #1 failed");
+            gxIC.Increment();
+            expectedIndxs[0] = 1;
+            expectedIndxs[1] = 0;
+            CopyIndexesToArray(ref actualIncremented, gxIC);
+            CollectionAssert.AreEqual(actualIncremented, actualIncremented, "Increment #2 failed");
+            gxIC.Increment();
+            expectedIndxs[0] = 2;
+            expectedIndxs[1] = 0;
+            CopyIndexesToArray(ref actualIncremented, gxIC);
+            CollectionAssert.AreEqual(actualIncremented, actualIncremented, "Increment #3 failed");
+            gxIC.Increment();
+            expectedIndxs[0] = 0;
+            expectedIndxs[1] = 1;
+            CopyIndexesToArray(ref actualIncremented, gxIC);
+            CollectionAssert.AreEqual(actualIncremented, actualIncremented, "Increment #4 failed");
+            gxIC.Increment();
+            expectedIndxs[0] = 1;
+            expectedIndxs[1] = 1;
+            CopyIndexesToArray(ref actualIncremented, gxIC);
+            CollectionAssert.AreEqual(expectedIndxs, actualIncremented, "Increment #5 failed.");
+            gxIC.Increment();
+            expectedIndxs[0] = 2;
+            expectedIndxs[1] = 1;
+            CopyIndexesToArray(ref actualIncremented, gxIC);
+            CollectionAssert.AreEqual(expectedIndxs, actualIncremented, "Increment #6 failed.");
+            gxIC.Increment();
+            expectedIndxs[0] = 0;
+            expectedIndxs[1] = 2;
+            CopyIndexesToArray(ref actualIncremented, gxIC);
+            CollectionAssert.AreEqual(expectedIndxs, actualIncremented, "Increment #7 failed.");
+            gxIC.Increment();
+            expectedIndxs[0] = 1;
+            expectedIndxs[1] = 2;
+            CopyIndexesToArray(ref actualIncremented, gxIC);
+            CollectionAssert.AreEqual(expectedIndxs, actualIncremented, "Increment #8 failed.");
+            gxIC.Increment();
+            expectedIndxs[0] = 2;
+            expectedIndxs[1] = 2;
+            CopyIndexesToArray(ref actualIncremented, gxIC);
+            CollectionAssert.AreEqual(expectedIndxs, actualIncremented, "Increment #9 failed.");
+            gxIC.Increment();
+            expectedIndxs[0] = 0;
+            expectedIndxs[1] = 0;
+            expectedIndxs[2] = 1;
+            CopyIndexesToArray(ref actualIncremented, gxIC);
+            CollectionAssert.AreEqual(expectedIndxs, actualIncremented, "Increment #10 failed.");
+            gxIC.Increment();
+            expectedIndxs[0] = 1;
+            expectedIndxs[1] = 0;
+            expectedIndxs[2] = 1;
+            CopyIndexesToArray(ref actualIncremented, gxIC);
+            CollectionAssert.AreEqual(expectedIndxs, actualIncremented, "Increment #11 failed.");
+            gxIC.Increment();
+            expectedIndxs[0] = 2;
+            expectedIndxs[1] = 0;
+            expectedIndxs[2] = 1;
+            CopyIndexesToArray(ref actualIncremented, gxIC);
+            CollectionAssert.AreEqual(expectedIndxs, actualIncremented, "Increment #12 failed.");
+            gxIC.Increment();
+            expectedIndxs[0] = 0;
+            expectedIndxs[1] = 1;
+            expectedIndxs[2] = 1;
+            CopyIndexesToArray(ref actualIncremented, gxIC);
+            CollectionAssert.AreEqual(expectedIndxs, actualIncremented, "Increment #13 failed.");
         }
     }
 }
