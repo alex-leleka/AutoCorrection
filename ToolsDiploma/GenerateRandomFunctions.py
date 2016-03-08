234431982ad14618befc7c37dd84154a8c26e15a
alex-leleka <aleksstork@gmail.com>
2016-03-08 23:31:49 +0200

    add distortion probabilities txt file generator to py script

--- ToolsDiploma/GenerateRandomFunctions.py
+++ ToolsDiploma/GenerateRandomFunctions.py
@@ -62,22 +62,85 @@ class BooleanFunction:
 			fileVar.write("\n")
 
 
+class InputDistortion:
+	""" Input distortions for logical network """
+	def __init__(self, inputbitsCount, outputbitsCount):
+		self.operandbitsCount = inputbitsCount
+		self.outputbitsCount = outputbitsCount
+		self.disttozero = []
+		self.disttoone = []
+		self.disttoinverse = []
+		self.zeroinput = []
+
+	def getrandom(a, b):
+		return round(random.uniform(a, b), 4)
+
+	def generatedistprob(self):
+		self.disttozero = []
+		self.disttoone = []
+		self.disttoinverse = []
+		self.zeroinput = []
+		for i in range(0, self.operandbitsCount):
+			self.disttozero.append(InputDistortion.getrandom(0.001, 0.01))
+			self.disttoone.append(InputDistortion.getrandom(0.001, 0.01))
+			self.disttoinverse.append(InputDistortion.getrandom(0.001, 0.01))
+			self.zeroinput.append(InputDistortion.getrandom(0.4, 0.6))
+
+	def writetofile(self, fileVar):
+		fileVar.write("# Input Distortions created by GenerateRandomFunctions.py \n")
+		fileVar.write("# inputbitsCount \n")
+		fileVar.write(str(self.operandbitsCount))
+		fileVar.write("\n")
+		fileVar.write("# outputbitsCount \n")
+		fileVar.write(str(self.outputbitsCount))
+		fileVar.write("\n")
+
+		fileVar.write("# dist to const zero : \n")
+		for value in self.disttozero:
+			fileVar.write(str(value) + " ")
+		fileVar.write("\n")
+
+		fileVar.write("# dist to const one : \n")
+		for value in self.disttoone:
+			fileVar.write(str(value) + " ")
+		fileVar.write("\n")
+
+		fileVar.write("# dist to inverse : \n")
+		for value in self.disttoinverse:
+			fileVar.write(str(value) + " ")
+		fileVar.write("\n")
+
+		fileVar.write("# input zero probability: \n")
+		for value in self.zeroinput:
+			fileVar.write(str(value) + " ")
+		fileVar.write("\n")
+
+
 class GenerationManager:
 	""" Creates functions and saves them on disk"""
-	def __init__(self, inputbitsCount, outputbitsCount, numberoffunctionstogenerate, filename):
+	def __init__(self, inputbitsCount, outputbitsCount, numberoffunctionstogenerate, funcfilename, numberofdistprobstogenerate = 0, distporbsfilename = "autogeninpdist"):
 		self.inputbitsCount = inputbitsCount
 		self.outputbitsCount = outputbitsCount
 		self.numberoffunctionstogenerate = numberoffunctionstogenerate
-		self.filename = filename
+		self.numberofdistprobstogenerate = numberofdistprobstogenerate
+		self.funcfilename = funcfilename
+		self.distporbsfilename = distporbsfilename + str(self.inputbitsCount) + "bit_order"
 
-	def run(self):
+	def genfunctionsrun(self):
 		for i in range(0, self.numberoffunctionstogenerate):
-			filename = self.filename + str(i) + ".txt"
+			filename = self.funcfilename + str(i) + ".txt"
 			bf = BooleanFunction(self.inputbitsCount, self.outputbitsCount)
 			bf.generatefunction()
 			with open(filename, 'w') as outfile:
 				bf.writetofile(outfile)
 
+	def gendistporbsrun(self):
+		for i in range(0, self.numberofdistprobstogenerate):
+			filename = self.distporbsfilename + str(i) + ".txt"
+			distprob = InputDistortion(self.inputbitsCount, self.outputbitsCount)
+			distprob.generatedistprob()
+			with open(filename, 'w') as outfile:
+				distprob.writetofile(outfile)
 
 # arguments 
 inputbitsCount = 5 # integer > 0
@@ -89,5 +152,6 @@ genSpeed = 1 # integer > 0
 
 # generate new boolean function
 
-m = GenerationManager(inputbitsCount, outputbitsCount, numberoffunctionstogenerate, filename)
-m.run()
+m = GenerationManager(inputbitsCount, outputbitsCount, numberoffunctionstogenerate, filename, 4)
+m.genfunctionsrun()
+m.gendistporbsrun()
